01af4b3fe99e81768a4bab82eb6726049908a60e
Boran Seckin <me@boranseckin.com>
2021-04-01 12:38:24 +0300

    Add inRange func

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,7 @@
 import crypto from 'crypto';
 
+import { M } from './node';
+
 export function serialize(data: any): Buffer {
     return Buffer.from(JSON.stringify(data));
 }
@@ -11,4 +13,43 @@ export function deserialize(data: Buffer): any {
 export function hash(data: string) {
     const sha1 = crypto.createHash('sha1').update(data);
     return sha1.digest('hex');
+}
+
+export function getFingerIndex(id: number, k: number) {
+    if (k < 1 && k > M) return -1;
+
+    return (id + (2 ** (k - 1))) % (2 ** M);
+}
+
+export function inRange(index: number, start: number, end: number, include: 'start' | 'end' | 'both' | 'none') {
+    switch (include) {
+    case 'start':
+        if (start < end) return (start <= index && index < end);
+        if (start > end) return !(start <= index && index < end);
+        if (start === end) return (index === end);
+        break;
+
+    case 'end':
+        if (start < end) return (start < index && index <= end);
+        if (start > end) return !(start < index && index <= end);
+        if (start === end) return (index === end);
+        break;
+
+    case 'both':
+        if (start < end) return (start <= index && index <= end);
+        if (start > end) return !(start <= index && index <= end);
+        if (start === end) return (index === end);
+        break;
+
+    case 'none':
+        if (start < end) return (start < index && index < end);
+        if (start > end) return !(start < index && index < end);
+        if (start === end) return (index !== end);
+        break;
+
+    default:
+        break;
+    }
+
+    return false;
 }
